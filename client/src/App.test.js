9677c2dcec7466f0b44cab4341cd818625f413f3
Ronan2140 <ronan2140@gmail.com>
2025-05-15 00:32:15 +0200

    fix front end tests and 2 tests steps

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 
-
+// dummy tests
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/S'inscrire/i);
   expect(linkElement).toBeInTheDocument();
 });
